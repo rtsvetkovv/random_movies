5f89943f133e75e5f9c43c6e441b18ad3a57b2d8
Роман Цветков <tsvetkovv22@gmail.com>
2019-01-12 00:14:28 +1000

    work check

--- App.js
+++ App.js
@@ -1,49 +1,68 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React, { Component } from 'react';
+import { Image } from 'react-native';
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body
+} from 'native-base';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import Test from './static/bg.jpg';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class CardShowcaseExample extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Header />
+        <Content>
+          <Card style={{ flex: 0 }}>
+            <CardItem>
+              <Left>
+                <Thumbnail source={Test} />
+                <Body>
+                  <Text>NativeBase</Text>
+                  <Text note>April 15, 2016</Text>
+                </Body>
+              </Left>
+            </CardItem>
+            <CardItem cardBody>
+              <Body>
+                <Image
+                  source={Test}
+                  style={{ height: 200, width: 400, flex: 1, marginBottom: 10 }}
+                />
+                <Text style={{ paddingHorizontal: 10 }}>
+                  "An apocalyptic story set in the furthest reaches of our
+                  planet, in a stark desert landscape where humanity is broken,
+                  and most everyone is crazed fighting for the necessities of
+                  life. Within this world exist two rebels on the run who just
+                  might be able to restore order. There's Max, a man of action
+                  and a man of few words, who seeks peace of mind following the
+                  loss of his wife and child in the aftermath of the chaos. And
+                  Furiosa, a woman of action and a woman who believes her path
+                  to survival may be achieved if she can make it across the
+                  desert back to her childhood homeland
+                </Text>
+              </Body>
+            </CardItem>
+            <CardItem>
+              <Left>
+                <Button transparent textStyle={{ color: '#87838B' }}>
+                  <Icon name="thumbs-up" />
+                  <Text>1,926</Text>
+                </Button>
+              </Left>
+            </CardItem>
+          </Card>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
