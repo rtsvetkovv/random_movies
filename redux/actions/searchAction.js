d628c0b22643ffb35a72851484c9879344435f73
Роман Цветков <tsvetkovv22@gmail.com>
2019-01-18 20:18:32 +1000

    Added .env

--- redux/actions/searchAction.js
+++ redux/actions/searchAction.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { BASE_URL } from "react-native-dotenv"
 
 import {
   FETCH_MOVIE_REQUEST_START,
   FETCH_MOVIE_REQUEST_SUCCESS,
   FETCH_MOVIE_REQUEST_ERROR
 } from '../actionTypes';
+
 import { Actions } from 'react-native-router-flux';
 
 const searchStart = () => ({
@@ -25,7 +27,7 @@ export const fetchMovie = page => dispatch => {
   dispatch(searchStart());
   axios
     .get(
-      `https://api.themoviedb.org/3/discover/movie?api_key=4499d31c1d8a60d9dad7b581321a53b2&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
+      `${BASE_URL}&language=ru-RU&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}`
     )
     .then(response => {
       dispatch(searchSuccess(response.data));
